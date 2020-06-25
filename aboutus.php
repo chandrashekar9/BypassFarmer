ebe998690f11303b53bf27b19acbe8847c417119
chandrashekar9 <67410912+chandrashekar9@users.noreply.github.com>
2020-06-25 14:01:54 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Enroute Farmer</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+ <style>
+ .details
+ {
+	 font-size:17px;
+	 text-align:center;
+ } </style>
+
+</head>
+
+
+<body>
+ 
+<!-- Sidebar -->
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+  <h3 class="w3-bar-item">Enroute Farmer</h3>
+
+</div>
+<!-- Page Content -->
+
+<div  style="margin-left:25%">
+ <div class="details">
+<!-- <p>This web page was created by Srinivas </p>
+ <p>Database is taken care by chandra shekar</p>
+ <p>Analatics is done by Tirupathi</p>
+ <p>Predictions are done by chandan</p>
+ <p>If any quries and information contact to the mobile number</p>
+ <p>7550170326...</p>
+ <p>Or mail us at chandrashekar9866460@gmail.com</p>
+ </div>
+	 </div>-->
+
+   
+
+      
+</body>
+</html>
